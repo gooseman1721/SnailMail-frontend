7f827fda9349e23b655d5adabaffdcb52d7c9fa4
Gooseman <theados123@gmail.com>
2022-09-22 20:17:30 +0200

    Made a simple front page with a button that makes a LoginModal pop-up

--- snailmail-frontend/src/App.jsx
+++ snailmail-frontend/src/App.jsx
@@ -1,21 +1,79 @@
 import './App.css';
-import { useState } from 'react';
-import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
+import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
-import Fade from '@mui/material/Fade';
-import { CssBaseline } from '@mui/material';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Modal from '@mui/material/Modal';
+import LoginModal from './components/LoginModal';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { blueGrey, grey, lightBlue, purple } from '@mui/material/colors';
+import { useState } from 'react';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blueGrey[500]
+    },
+    secondary: {
+      main: purple[200]
+    },
+    background: {
+      default: "#fafafa"
+    }
+  },
+})
 
 function App() {
 
+  const [openLoginModal, setOpenLoginModal] = useState(false);
+  const handleOpenLoginModal = () => setOpenLoginModal(true);
+  const handleCloseLoginModal = () => setOpenLoginModal(false);
+
   return (
     <div className="App">
-      <CssBaseline enableColorScheme />
-      <div className='appContent'>
-        <h1>Sample text</h1>
-        
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container >
+          <Box className="basicBox">
+
+            <Stack justifyContent={'flex-end'} spacing={"5vw"} direction='row'>
+              <Typography variant='h1' sx={{ flexGrow: 1 }}>
+                SnailMail
+              </Typography>
+              <Button variant='contained' sx={{ alignSelf: 'center' }} onClick={handleOpenLoginModal}>Sign in</Button>
+            </Stack>
+           
+          </Box>
+          <Box sx={{py: "3vw"}}>
+            <Stack>
+              <Typography variant='h6' sx={{ alignSelf: 'center' }}>
+                Enter the world.. of SnailMail
+              </Typography>
+              <Typography sx={{ alignSelf: 'center' }}>
+                Snailmail is plepleplpe and Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ante libero, aliquam nec risus sed, placerat sodales nisl. Mauris scelerisque venenatis commodo. Donec ut nunc ex. Donec non ultrices erat, vel porttitor metus. Aenean gravida, nisi in viverra convallis, nibh lorem rhoncus diam, quis luctus lectus sem nec eros. Nunc vitae tellus urna. Sed id eleifend sapien.
+              </Typography>
+              <Stack direction='row' spacing={"5vw"} sx={{ alignSelf: 'center', py: '2vw' }}>
+                <Button variant='contained' >ENTER</Button>
+                <Button variant='outlined' >SOME OTHER OPTION</Button>
+              </Stack>
+            </Stack>
+
+
+          </Box>
+          <Box sx={{py: "1vw"}}>
+            <Typography variant='h2'>
+              A demo here?
+            </Typography>
+          </Box>
+          <LoginModal open={openLoginModal} close={handleCloseLoginModal} />
+        </Container>
+      </ThemeProvider>
+      
+
 
 
     </div>
---
+++ snailmail-frontend/src/components/LoginModal.jsx
@@ -0,0 +1,50 @@
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography  from "@mui/material/Typography";
+import { TextField } from "@mui/material";
+import { Stack } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '400',
+    bgcolor: 'background.paper',
+    border: '2px solid rgba(88, 121, 126, 0.205)',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: '2%',
+};
+
+export default function LoginModal(props) {
+  return (
+    <div>
+        <Modal
+            open={props.open}
+            onClose={props.close}    
+        >
+            <Box sx={style}>
+                <Stack>
+                      <Typography variant="h6" component="h2">
+                          Username
+                      </Typography>
+                      <TextField></TextField>
+                      <Typography variant="h6" component="h2">
+                          Password
+                      </Typography>
+                      <TextField></TextField>
+                      <Stack mt='1em' direction='row' justifyContent='flex-center' gap='1em'>
+                          <Button variant="contained">Log in</Button>
+                          <Button variant="outlined">Not registered?</Button>
+                      </Stack>
+                </Stack>
+
+                       
+                       
+            </Box>
+        </Modal>
+    </div>
+  )
+}

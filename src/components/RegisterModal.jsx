cb362258ddde18b7441514950dc3a0d2d3567422
Gooseman <theados123@gmail.com>
2022-09-22 20:27:08 +0200

    folder cleanup

---
+++ src/components/RegisterModal.jsx
@@ -0,0 +1,12 @@
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
+import { Stack } from "@mui/material";
+import Button from "@mui/material/Button";
+
+export default function RegisterModal() {
+  return (
+    <div>RegisterModal</div>
+  )
+}

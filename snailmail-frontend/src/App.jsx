6e31a6e57d3dce39e2ca20e0e8649228cddabe97
Gooseman <theados123@gmail.com>
2022-09-22 16:12:56 +0200

    first commit

---
+++ snailmail-frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Fade from '@mui/material/Fade';
+import { CssBaseline } from '@mui/material';
+
+function App() {
+
+  return (
+    <div className="App">
+      <CssBaseline enableColorScheme />
+      <div className='appContent'>
+        <h1>Sample text</h1>
+        
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
